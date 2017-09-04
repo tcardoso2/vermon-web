e1a31e9628dd141b447ac40815da947bd583152f
Tiago Cardoso <tcardoso@gmail.com>
2017-09-04 21:14:09 +0800

    Adding support for plugins

---
+++ test/internal-extension-specs.js
@@ -0,0 +1,50 @@
+  /*****************************************************
+ * Internal tests
+ * What are internal tests?
+ * As this is a npm package, it should be tested from
+ * a package context, so I'll use "interal" preffix
+ * for tests which are NOT using the npm tarball pack
+ * For all others, the test should obviously include
+ * something like:
+ * var md = require('t-motion-detector');
+ *****************************************************/
+
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+let chaiAsPromised = require("chai-as-promised");
+let should = chai.should();
+let fs = require('fs');
+let ent = require('../Entities');
+let main = require('../main');
+let events = require('events');
+let express = require('express');
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
+
+before(function(done) {
+  done();
+});
+
+after(function(done) {
+  done();
+});
+
+describe("When creating a t-motion-detector-cli extension, ", function() {
+
+  it('It should be able to access the t-motion-detector Entities via the "$" accessor.', function (done) {
+     //Prepare
+    let Entities = main.$.Entities;
+    done();
+  });
+  it('The developer should link both libraries using the AddPlugin function.', function (done) {
+     //Prepare
+    main.$.Reset();
+    let alternativeConfig = new main.$.Config("/test/config_express_test3.js");
+
+    main.$.StartWithConfig(alternativeConfig, (e, d, n, f)=>{
+      
+      done();
+    });
+  });
+});
