027af85086763598b2fb6c128fb7b28bec481968
Tiago Cardoso <tiago.cardoso@jeppesen.com>
2018-09-30 06:59:20 +0000

    Mocha badge and mocha badge reporter script updates

--- scripts/create_badge.sh
+++ scripts/create_badge.sh
@@ -6,4 +6,4 @@ export MOCHA_BADGE_KO_COLOR=orange
 export MOCHA_BADGE_STYLE=flat
 
 #Reporter
-mocha test/*.js --reporter mocha-reporter-badge | sed -n -e '/<svg/,$p' > badge.svg
+mocha --exit test/*.js --reporter mocha-reporter-badge | sed -n -e '/<svg/,$p' > badge.svg
