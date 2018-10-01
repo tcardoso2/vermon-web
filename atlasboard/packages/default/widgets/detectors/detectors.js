66f1418614b8d4f065ac821e2d7d7078fb01a2a1
Tiago Cardoso <tcardoso@gmail.com>
2018-10-01 16:14:32 +0800

    Update on unnecessary code for detectors

--- atlasboard/packages/default/widgets/detectors/detectors.js
+++ atlasboard/packages/default/widgets/detectors/detectors.js
@@ -37,54 +37,8 @@ widget = {
         parent.onItemUpdate($(`#${i}`, el), data[value]);
       }
     }
-    return;
-
-    //To remove after confirming it is ok
-    parent.onItemUpdate($('#data_sala', el), 
-      data["mihome.0.devices.sensor_ht_158d000208fc30.temperature"]);
-    parent.onItemUpdate($('#data_batcave', el), 
-      data["mihome.0.devices.sensor_ht_158d00020db386.temperature"]);
-    parent.onItemUpdate($('#data_bedroom', el), 
-      data["mihome.0.devices.sensor_ht_158d0001f54051.temperature"]);
-    parent.onItemUpdate($('#data_sala_humidity', el), 
-      data["mihome.0.devices.sensor_ht_158d000208fc30.humidity"]);
-    parent.onItemUpdate($('#data_batcave_humidity', el), 
-      data["mihome.0.devices.sensor_ht_158d00020db386.humidity"]);
-    parent.onItemUpdate($('#data_bedroom_humidity', el), 
-      data["mihome.0.devices.sensor_ht_158d0001f54051.humidity"]);
-    parent.onItemUpdate($('#s_mijia_connected', el), 
-      data["system.adapter.mihome.0.connected"], "gateway", "");
-    parent.onItemUpdate($('#s_data_street_door', el), 
-      data["mihome.0.devices.magnet_158d00016c1eea.state"], "open", "closed");
-    parent.onItemUpdate($('#s_data_kitchen_freezer', el), 
-      data["mihome.0.devices.magnet_158d00020468a4.state"], "open", "closed");
-    parent.onItemUpdate($('#s_data_batcave_closet', el), 
-      data["mihome.0.devices.magnet_158d00022d2f5d.state"], "open", "closed");
-    parent.onItemUpdate($('#s_data_balcony_door', el), 
-      data["mihome.0.devices.magnet_158d00022da804.state"], "open", "closed");
-    parent.onItemUpdate($('#s_data_balcony_person', el), 
-      data["mihome.0.devices.sensor_motion_aq2_158d000272bd25.state"], "person", "");
-    parent.onItemUpdate($('#s_data_batcave_person', el), 
-      data["mihome.0.devices.sensor_motion_aq2_158d000237aa80.state"], "person", "");
-    parent.onItemUpdate($('#s_data_batcave_smoke', el), 
-      data["mihome.0.devices.smoke_158d0001db94c4.state"], "smoke", "");
-    parent.onItemUpdate($('#s_data_bedroom_person', el), 
-      data["mihome.0.devices.sensor_motion_aq2_158d000276d3fa.state"], "person", "");
-    parent.onItemUpdate($('#s_data_batcave_lamp', el), 
-      data["yeelight-2.0.yeelight-2.0.yeelight-2.0.stripe-Light_strip"], "lamp", "");
-    parent.onItemUpdate($('#data_balcony', el), 
-      data["mihome.0.devices.sensor_ht_158d000237950b.temperature"]);
-    parent.onItemUpdate($('#data_balcony_humidity', el), 
-      data["mihome.0.devices.sensor_ht_158d000237950b.humidity"]);
-    parent.onItemUpdate($('#data_kitchen', el), 
-      data["mihome.0.devices.sensor_ht_158d0002379581.temperature"]);
-    parent.onItemUpdate($('#data_kitchen_humidity', el), 
-      data["mihome.0.devices.sensor_ht_158d0002379581.humidity"]);
-
     console.log(">>>> Finished updating socket data...");
   },
-  //mihome.0.devices.smoke_158d0001db94c4
-  //mihome.0.devices.switch_158d000255c528
 
   onItemUpdate(el, data, classTrue, classFalse){
     if (data){
